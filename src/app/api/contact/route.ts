ae1eb52238513feed6cb4e99bfdcf5c3c909c6cc
YDayeon <88532969+YDayeon@users.noreply.github.com>
2023-04-27 18:00:47 +0900

    modify contact API  handler

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,19 +1,16 @@
-import { sendMail } from '@/app/service/contact';
+import { sendMail } from "@/app/service/contact";
 
 export async function POST(request: Request) {
-  console.log('body', request);
-  return new Response('✅Message delivered.', {
-    status: 200,
-  });
-  // try {
-  //   console.log('request', request);
-  //   await sendMail(request.body);
-  // } catch (error) {
-  //   return new Response('Message sending failed. Please try again.', {
-  //     status: 500,
-  //   });
-  // }
-  // return new Response('✅Message delivered.', {
-  //   status: 200,
-  // });
+  try {
+    const body = await request.json();
+    const result = await sendMail(body);
+    console.log("result", result);
+    return new Response("✅Message delivered.", {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response("Message sending failed. Please try again.", {
+      status: 500,
+    });
+  }
 }
