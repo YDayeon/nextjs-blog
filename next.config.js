b2b68c3ed2430f84554ced7929a7804f83df8035
Dayeon Yoon <yday1223@gmail.com>
2023-05-05 12:50:14 +0900

    modify config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,13 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ["picsum.photos", "api.api-ninjas.com"],
+    domains: ['picsum.photos', 'api.api-ninjas.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
   },
 };
 
