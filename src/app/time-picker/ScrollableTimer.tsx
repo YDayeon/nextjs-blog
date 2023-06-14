c76d7f24a8eaec699a3b75a430bc76a7e803499e
YDayeon <88532969+YDayeon@users.noreply.github.com>
2023-06-14 18:00:52 +0900

    almost complete

--- src/app/time-picker/ScrollableTimer.tsx
+++ src/app/time-picker/ScrollableTimer.tsx
@@ -19,76 +19,103 @@ const time = new Array(24).fill(0).map((el, i) => el + i);
 const minute = new Array(60).fill(0).map((el, i) => el + i);
 
 export default function ScrollableTimer() {
-  const elements = useRef<null[] | HTMLLIElement[]>([]);
+  const daysElement = useRef<null[] | HTMLLIElement[]>([]);
+  const timeElement = useRef<null[] | HTMLLIElement[]>([]);
+  const minuteElement = useRef<null[] | HTMLLIElement[]>([]);
   const root = useRef<HTMLDivElement>(null);
   const observer = useRef<IntersectionObserver | null>(null);
 
   useEffect(() => {
     if (!!root.current) {
-      console.log(root.current.offsetHeight - 24);
+      let debounceTimer: NodeJS.Timeout;
       observer.current = new IntersectionObserver(
         (entries) => {
           entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              entry.target.scrollIntoView({
-                behavior: 'smooth',
-                block: 'center',
-                inline: 'center',
-              });
-              console.log(entry.target, 'isIntersecting!!');
+            if (entry.isIntersecting && entry.intersectionRatio > 0) {
+              debounceTimer = setTimeout(() => {
+                entry.target.scrollIntoView({
+                  behavior: 'smooth',
+                  block: 'center',
+                  inline: 'center',
+                });
+              }, 100);
+            } else {
+              clearTimeout(debounceTimer);
             }
           });
         },
         {
           root: root.current,
-          rootMargin: '-60px 0px -60px 0px',
-          threshold: 0.6,
+          rootMargin: '-56px 0px -56px 0px',
+          threshold: 0.4,
         }
       );
+    }
+  }, [root]);
 
-      elements.current?.forEach((el) => {
+  useEffect(() => {
+    if (!!observer.current) {
+      daysElement.current?.forEach((el) => {
+        if (el) {
+          observer.current?.observe(el);
+        }
+      });
+      timeElement.current?.forEach((el) => {
+        if (el) {
+          observer.current?.observe(el);
+        }
+      });
+      minuteElement.current?.forEach((el) => {
         if (el) {
           observer.current?.observe(el);
         }
       });
     }
-  }, [elements, root]);
+  }, [observer.current]);
 
   return (
     <div
-      className='w-full h-36 overflow-hidden bg-gray-600 my-10 flex justify-around relative'
+      className='w-full h-36 overflow-hidden bg-gray-600 my-10 flex relative px-5 text-white'
       ref={root}
     >
-      <ul className='overflow-scroll scrollbar-hide z-10 py-16'>
+      <ul className='overflow-scroll scrollbar-hide z-10 py-16 w-full basis-1/3 pl-6'>
         {days.map((el, i) => (
           <li
-            ref={(el) => (elements.current[i] = el)}
+            ref={(el) => (daysElement.current[i] = el)}
             key={el}
-            className='text-center text-base h-6'
+            className='h-6'
           >
             {el}
           </li>
         ))}
       </ul>
-      <ul className='overflow-scroll scrollbar-hide z-10 py-16'>
+      <ul className='overflow-scroll scrollbar-hide z-10 py-16 w-full text-center pr-5 basis-1/3'>
         {time.map((el, i) => (
-          <li key={el} className='text-center text-base h-6'>
+          <li
+            ref={(el) => (timeElement.current[i] = el)}
+            key={el}
+            className='h-6'
+          >
             {el}
           </li>
         ))}
       </ul>
-      <ul className='overflow-scroll scrollbar-hide z-10 py-16'>
+      <ul className='overflow-scroll scrollbar-hide z-10 py-16 w-full text-end basis-1/3 pr-10'>
         {minute.map((el, i) => (
-          <li key={el} className='text-center text-base h-6'>
+          <li
+            ref={(el) => (minuteElement.current[i] = el)}
+            key={el}
+            className='h-6'
+          >
             {el}
           </li>
         ))}
       </ul>
-      <div className='w-full h-full absolute top-0 flex items-center z-0 px-3'>
-        <div className='h-6 w-full bg-white/40 rounded-md flex'>
-          <span className='basis-1/3 text-end pr-5'>요일</span>
-          <span className='basis-1/3 text-end pr-6'>시</span>
-          <span className='basis-1/3 text-end pr-5'>분</span>
+      <div className='w-full h-full absolute top-0 left-0 flex items-center z-0 px-5'>
+        <div className='h-8 w-full bg-white/40 rounded-md flex items-center font-bold'>
+          <span className='basis-1/3 pl-11'>요일</span>
+          <span className='basis-1/3 text-center pl-5'>시</span>
+          <span className='basis-1/3 pr-5 text-end'>분</span>
         </div>
       </div>
     </div>
